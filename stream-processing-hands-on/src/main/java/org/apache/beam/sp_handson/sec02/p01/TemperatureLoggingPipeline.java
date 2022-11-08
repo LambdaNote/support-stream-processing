ad552dce71de31727effb75bcad88aa58b970912
Sho Nakatani <lay.sakura@gmail.com>
2022-11-09 05:25:20 +0900

    feat: Kafka read/write を試した。ニュルニュルという程ではなかった

--- stream-processing-hands-on/src/main/java/org/apache/beam/sp_handson/sec02/p01/TemperatureLoggingPipeline.java
+++ stream-processing-hands-on/src/main/java/org/apache/beam/sp_handson/sec02/p01/TemperatureLoggingPipeline.java
@@ -1,25 +1,34 @@
 package org.apache.beam.sp_handson.sec02.p01;
 
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.mqtt.MqttIO;
+import org.apache.beam.sdk.io.kafka.KafkaIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 public class TemperatureLoggingPipeline {
   public static void main(String[] args) {
     Pipeline p = Pipeline.create(
         PipelineOptionsFactory.fromArgs(args).withValidation().create());
 
-    PCollection<byte[]> mqttInput = p.apply(MqttIO.read()
-        .withConnectionConfiguration(MqttIO.ConnectionConfiguration.create(
-            "tcp://127.0.0.1:1883",
-            "weather/kushiro")));
+    PCollection<KV<Long, String>> kafkaInput = p.apply(
+        KafkaIO.<Long, String>read()
+            .withBootstrapServers("localhost:9092")
+            .withTopic("weather-kushiro")
+            .withKeyDeserializer(LongDeserializer.class)
+            .withValueDeserializer(StringDeserializer.class)
+            .withoutMetadata() // KafkaRecord -> KV
+    );
 
-    mqttInput.apply(
-        MqttIO.write()
-            .withConnectionConfiguration(MqttIO.ConnectionConfiguration.create(
-                "tcp://127.0.0.1:1883",
-                "beam/out")));
+    kafkaInput.apply(KafkaIO.<Long, String>write()
+        .withBootstrapServers("localhost:9092")
+        .withTopic("beam-out")
+        .withKeySerializer(LongSerializer.class)
+        .withValueSerializer(StringSerializer.class));
 
     p.run();
   }

20db173c259632c3c7cf819471b0779514b0f104
Sho Nakatani <lay.sakura@gmail.com>
2022-11-10 12:32:03 +0900

    fix: JavaFieldSchemaにしたら動く

--- stream-processing-hands-on/src/main/java/org/apache/beam/sp_handson/sec02/p02/Weather.java
+++ stream-processing-hands-on/src/main/java/org/apache/beam/sp_handson/sec02/p02/Weather.java
@@ -1,18 +1,17 @@
 package org.apache.beam.sp_handson.sec02.p02;
 
-import org.apache.beam.sdk.schemas.JavaBeanSchema;
+import org.apache.beam.sdk.schemas.JavaFieldSchema;
 import org.apache.beam.sdk.schemas.annotations.DefaultSchema;
 import org.apache.beam.sdk.schemas.annotations.SchemaCreate;
-import org.joda.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 // アメダス気象情報
-@DefaultSchema(JavaBeanSchema.class)
+@DefaultSchema(JavaFieldSchema.class)
 public class Weather {
-    // 観測日時 (event timeとして利用)
-    public final Instant timestamp;
+    // 観測日時
+    public final String timestamp;
     // 気温 [℃]
     public final float temperatureC;
     // 降水量 [mm]
@@ -24,12 +23,12 @@ public class Weather {
             @JsonProperty("timestamp") String timestamp,
             @JsonProperty("temperature [°C]") float temperatureC,
             @JsonProperty("rainfall [mm]") float rainfallMm) {
-        this.timestamp = Instant.parse(timestamp);
+        this.timestamp = timestamp;
         this.temperatureC = temperatureC;
         this.rainfallMm = rainfallMm;
     }
 
     public String toLine() {
-        return this.timestamp.toString() + "\ttemperature:" + this.temperatureC + "\trainfall:" + this.rainfallMm;
+        return this.timestamp + "\ttemperature:" + this.temperatureC + "\trainfall:" + this.rainfallMm;
     }
 }

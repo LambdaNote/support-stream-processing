e16a679d3f6d1f97e654e872f842b89753b5437b
Sho Nakatani <lay.sakura@gmail.com>
2022-11-11 05:29:33 +0900

    fix: WARNログが出ていたのでWeatherに一致比較を提供

--- stream-processing-hands-on/src/main/java/org/apache/beam/sp_handson/sec02/p02/Weather.java
+++ stream-processing-hands-on/src/main/java/org/apache/beam/sp_handson/sec02/p02/Weather.java
@@ -7,8 +7,11 @@ import org.apache.beam.sdk.schemas.annotations.SchemaCreate;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.EqualsAndHashCode;
+
 // アメダス気象情報
 @DefaultSchema(JavaFieldSchema.class) // BeamのSchemaとする
+@EqualsAndHashCode // イベント同士の妥当な一致比較を提供
 public class Weather {
     // 観測日時 (RFC-3339 形式)
     public final String timestamp;

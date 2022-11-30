24431347abbbd9720375d917bd0de336b9739f5e
Sho Nakatani <lay.sakura@gmail.com>
2022-12-01 05:02:47 +0900

    docs: コメント追加

--- stream-processing-hands-on/src/main/java/org/apache/beam/sp_handson/sec03/p01/AvgTemperaturePerDayPipeline.java
+++ stream-processing-hands-on/src/main/java/org/apache/beam/sp_handson/sec03/p01/AvgTemperaturePerDayPipeline.java
@@ -25,78 +25,82 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class AvgTemperaturePerDayPipeline {
 
-    // アメダス観測のJSON文字列をWeatherクラスにマッピングするため
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+        // アメダス観測のJSON文字列をWeatherクラスにマッピングするため
+        private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static void main(String[] args) {
-        Pipeline p = Pipeline.create(
-                PipelineOptionsFactory.fromArgs(args).withValidation().create());
+        public static void main(String[] args) {
+                Pipeline p = Pipeline.create(
+                                PipelineOptionsFactory.fromArgs(args).withValidation().create());
 
-        // Kafkaソース入力
-        PCollection<KV<Long, String>> kafkaInput = p.apply(
-                KafkaIO.<Long, String>read()
-                        .withBootstrapServers("localhost:9092")
-                        .withTopic("weather-kushiro")
-                        .withKeyDeserializer(LongDeserializer.class)
-                        .withValueDeserializer(StringDeserializer.class)
-                        .withTimestampPolicyFactory(new WeatherTimestampPolicyFactory<>())
-                        .withReadCommitted()
-                        .withoutMetadata());
+                // Kafkaソース入力
+                PCollection<KV<Long, String>> kafkaInput = p.apply(
+                                KafkaIO.<Long, String>read()
+                                                .withBootstrapServers("localhost:9092")
+                                                .withTopic("weather-kushiro")
+                                                .withKeyDeserializer(LongDeserializer.class)
+                                                .withValueDeserializer(StringDeserializer.class)
+                                                // Event time割当を行う
+                                                .withTimestampPolicyFactory(new WeatherTimestampPolicyFactory<>())
+                                                .withReadCommitted()
+                                                .withoutMetadata());
 
-        // KV<Long, String> のバリュー部分をJSONとしてパースし、Weatherクラスにマッピング。
-        // 合わせて、イベントタイムも設定。
-        PCollection<Weather> weather = kafkaInput.apply(
-                ParDo.of(new DoFn<KV<Long, String>, Weather>() {
-                    @ProcessElement
-                    public void processElement(@Element KV<Long, String> rawWeather, OutputReceiver<Weather> out)
-                            throws JsonProcessingException {
-                        String jsonWeather = rawWeather.getValue();
-                        Weather weather = objectMapper.readValue(jsonWeather, Weather.class);
-                        out.output(weather);
-                    }
-                }));
+                // KV<Long, String> のバリュー部分をJSONとしてパースし、Weatherクラスにマッピング。
+                // 合わせて、イベントタイムも設定。
+                PCollection<Weather> weather = kafkaInput.apply(
+                                ParDo.of(new DoFn<KV<Long, String>, Weather>() {
+                                        @ProcessElement
+                                        public void processElement(@Element KV<Long, String> rawWeather,
+                                                        OutputReceiver<Weather> out)
+                                                        throws JsonProcessingException {
+                                                String jsonWeather = rawWeather.getValue();
+                                                Weather weather = objectMapper.readValue(jsonWeather, Weather.class);
+                                                out.output(weather);
+                                        }
+                                }));
 
-        // 気象情報から日付と気温だけを抽出
-        PCollection<KV<String, Float>> temperatureWithDate = weather.apply(
-                ParDo.of(new DoFn<Weather, KV<String, Float>>() {
-                    @ProcessElement
-                    public void processElement(
-                            @Element Weather w,
-                            OutputReceiver<KV<String, Float>> out) {
-                        // 日本時間での日付
-                        String date = Instant.parse(w.timestamp).toDateTime(DateTimeZone.forID("+09:00"))
-                                .toLocalDate().toString();
-                        out.output(KV.of(date, w.temperatureC));
-                    }
-                }));
+                // 気象情報から日付と気温だけを抽出
+                PCollection<KV<String, Float>> temperatureWithDate = weather.apply(
+                                ParDo.of(new DoFn<Weather, KV<String, Float>>() {
+                                        @ProcessElement
+                                        public void processElement(
+                                                        @Element Weather w,
+                                                        OutputReceiver<KV<String, Float>> out) {
+                                                // 日本時間での日付
+                                                String date = Instant.parse(w.timestamp)
+                                                                .toDateTime(DateTimeZone.forID("+09:00"))
+                                                                .toLocalDate().toString();
+                                                out.output(KV.of(date, w.temperatureC));
+                                        }
+                                }));
 
-        // Event time軸で1日毎の固定幅ウィンドウを構築
-        PCollection<KV<String, Float>> windowedTemperatureWithDate = temperatureWithDate.apply(
-                Window.<KV<String, Float>>into(
-                        FixedWindows.of(Duration.standardDays(1))
-                                // ウィンドウの開始日時はUTC原点の0時になるので日本時間の0時にずらす。
-                                // -9時間ずらしたいが、負数指定ができないので 24 - 9 = 15 時間ずらす。
-                                .withOffset(Duration.standardHours(15))));
+                // Event time軸で1日毎の固定幅ウィンドウを構築
+                PCollection<KV<String, Float>> windowedTemperatureWithDate = temperatureWithDate.apply(
+                                Window.<KV<String, Float>>into(
+                                                FixedWindows.of(Duration.standardDays(1))
+                                                                // ウィンドウの開始日時はUTC原点の0時になるので日本時間の0時にずらす。
+                                                                // -9時間ずらしたいが、負数指定ができないので 24 - 9 = 15 時間ずらす。
+                                                                .withOffset(Duration.standardHours(15))));
 
-        // 日付毎に、各ウィンドウで気温の平均値を計算
-        PCollection<KV<String, Double>> meanTemperature = windowedTemperatureWithDate.apply(
-                Mean.<String, Float>perKey());
+                // 日付毎に、各ウィンドウで気温の平均値を計算
+                PCollection<KV<String, Double>> meanTemperature = windowedTemperatureWithDate.apply(
+                                Mean.<String, Float>perKey());
 
-        // フォーマットして文字列化
-        PCollection<String> meanTemperatureLine = meanTemperature.apply(
-                MapElements
-                        .into(TypeDescriptors.strings())
-                        .via(meanByDate -> "date:" + meanByDate.getKey() + "\tmeanTemperature(daily):"
-                                + meanByDate.getValue()));
+                // フォーマットして文字列化
+                PCollection<String> meanTemperatureLine = meanTemperature.apply(
+                                MapElements
+                                                .into(TypeDescriptors.strings())
+                                                .via(meanByDate -> "date:" + meanByDate.getKey()
+                                                                + "\tmeanTemperature(daily):"
+                                                                + meanByDate.getValue()));
 
-        // Kafkaシンク出力
-        meanTemperatureLine.apply(
-                KafkaIO.<Void, String>write()
-                        .withBootstrapServers("localhost:9092")
-                        .withTopic("beam-out")
-                        .withValueSerializer(StringSerializer.class)
-                        .values());
+                // Kafkaシンク出力
+                meanTemperatureLine.apply(
+                                KafkaIO.<Void, String>write()
+                                                .withBootstrapServers("localhost:9092")
+                                                .withTopic("beam-out")
+                                                .withValueSerializer(StringSerializer.class)
+                                                .values());
 
-        p.run();
-    }
+                p.run();
+        }
 }
